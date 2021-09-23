37e813580d6936b0b6da365f3aae290d2b2b3618
sidarhalman <sidarhalman93@gmail.com>
2021-09-23 11:03:22 +0200

    add CreateRandomnumber(x,y); function

--- src/index.js
+++ src/index.js
@@ -15,11 +15,29 @@ const path = require('path'); // Path, zum arbeiten mit dateipfaden
  */
 function getWords()
 {
+    //wir lesen eine json 
     const json = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'words.json'), 'utf-8'));
 
+    // wir geben dir worte zurück
     return json;
 };
 
 // - Eine function um dir zufällige Nummer hinter dem Namen zu generieren
+
+/**
+ * @description Creates a random number accordeing to a minimal and maximal value
+ * @param { number } minNumber 
+ * @param { number } maxNumber 
+ * @returns { number }
+ */
+function createRandomNumber(minNumber, maxNumber)
+{
+    //Wir erschafen eine zufallzahl Number hinter dem namen generieren
+    const num = Math.floor(Math.random() * (maxNumber - minNumber) + minNumber);
+
+    // wir geben die zufällige numer zurück
+    console.log(num);
+}
+createRandomNumber(1, 20000)
 // - Eine function mit der grose buchstabel
 // - eine function mit der wir den benutzernamen generieren und auf die ergebnisse der anderen functioniren zugreifen
